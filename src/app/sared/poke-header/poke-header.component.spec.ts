68e6c746c3722828d454f322aea2cdcfca488fab
jocinei <jocineidallacosta@hotmail.com>
2021-10-16 21:56:31 -0300

    ajuste de versão

--- src/app/sared/poke-header/poke-header.component.spec.ts
+++ src/app/sared/poke-header/poke-header.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { PokeHeaderComponent } from './poke-header.component';
 
@@ -6,7 +6,7 @@ describe('PokeHeaderComponent', () => {
   let component: PokeHeaderComponent;
   let fixture: ComponentFixture<PokeHeaderComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ PokeHeaderComponent ]
     })
